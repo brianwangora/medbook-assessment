aff1c7cdf4ddec7234009b870787c292e81edc76
Brian Wangora <brian.wangora@atlancis.com>
2026-08-21 14:47:39 +0300

    Setup Database seeder to add seed data to db to easily reproduce the scenario

---
+++ medbook-backend/database/seeders/CustomerSeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Customer;
+use Illuminate\Database\Seeder;
+
+class CustomerSeeder extends Seeder
+{
+    public function run(): void
+    {
+        Customer::create([
+            'name' => 'Peter',
+            'service' => 'Consultation',
+            'arrived_at' => today()->setTime(9, 45),
+            'original_priority' => 'Normal',
+        ]);
+
+        Customer::create([
+            'name' => 'Mary',
+            'service' => 'Consultation',
+            'arrived_at' => today()->setTime(11, 1),
+            'original_priority' => 'Emergency',
+        ]);
+
+        Customer::create([
+            'name' => 'John',
+            'service' => 'Consultation',
+            'arrived_at' => today()->setTime(11, 4),
+            'original_priority' => 'Emergency',
+        ]);
+
+        Customer::create([
+            'name' => 'Susan',
+            'service' => 'Consultation',
+            'arrived_at' => today()->setTime(10, 25),
+            'original_priority' => 'Priority',
+        ]);
+
+        Customer::create([
+            'name' => 'Daniel',
+            'service' => 'Consultation',
+            'arrived_at' => today()->setTime(10, 50),
+            'original_priority' => 'Normal',
+        ]);
+    }
+}
